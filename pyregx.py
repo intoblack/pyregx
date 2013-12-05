7e32eb1fb80713bec09b923a6907efa55885e68a
lixuze <aihai2800@126.com>
2013-12-05 15:14:59 +0800

    添加 数字符号 == @！= 支持

--- pyregx.py
+++ pyregx.py
@@ -42,6 +42,17 @@ class PyRe(object):
         else:
             return False
 
+    def __eq__(self, data):
+        if data and isinstance(data, (str, unicode)):
+            if self.equal(data):
+                return True
+            return False
+        else:
+            raise TypeError, 'data must be str or unicode!'
+
+    def __ne__(self, data):
+        return not self.__eq__(data)
+
     # 数据中包含正则表达式数据
     # 如果有数据符合正则
     # 返回 True
@@ -62,46 +73,40 @@ class PyRe(object):
         return find_data
 
 
-class  PPattern(object):
-    
+class PPattern(object):
+
     __pattern_dict = {}
 
-
-
-    def __setattr__(self,key,value):
-        if key and value and isinstance(key,(str)) and isinstance(value,(PyRe)):
+    def __setattr__(self, key, value):
+        if key and value and isinstance(key, (str)) and isinstance(value, (PyRe)):
             if not self.__pattern_dict.has_key(key):
                 self.__pattern_dict[key] = value
         else:
-            raise TypeError,'key must string and value is PyRe'
+            raise TypeError, 'key must string and value is PyRe'
 
-    def __getattr__(self,key):
-        if isinstance(key , (str)):
+    def __getattr__(self, key):
+        if isinstance(key, (str)):
             if not self.__pattern_dict.has_key(key):
                 if regdict.regx_dict.has_key(key.upper()):
-                    self.__pattern_dict[key] = PyRe(regdict.regx_dict[key.upper()])
+                    self.__pattern_dict[key] = PyRe(
+                        regdict.regx_dict[key.upper()])
                 else:
-                    raise NotImplementedError,'regx_dict 没有正则'
+                    raise NotImplementedError, 'regx_dict 没有正则'
             return self.__pattern_dict[key]
-        raise TypeError,'key must be sting '
+        raise TypeError, 'key must be sting '
 
-
-    def __delattr__(self ,key):
-        if key and isinstance(key ,str):
+    def __delattr__(self, key):
+        if key and isinstance(key, str):
             if self.__pattern_dict.has_key(key):
                 del self.__pattern_dict[key]
         else:
-            raise TypeError,'key must be string'
-
-        
+            raise TypeError, 'key must be string'
 
 
 if __name__ == "__main__":
-    
+
     p = PPattern()
-    print p.IP.equal('123.145.23.45.22.1144')
+    print p.IP != '123.145.23.45'
     del p.IP
     print p.mail.find_iter('xxx1212@126.com')
     print p.html_tag.find_iter("<html xx=11>")
-
-
